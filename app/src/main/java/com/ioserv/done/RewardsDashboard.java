5a6bb38f1a718725b4b8ec1b09f30ad0c4063094
MonicaVargasY <105678951+MonicaVargasY@users.noreply.github.com>
2022-07-15 15:41:49 -0400

    Add rewards activity

---
+++ app/src/main/java/com/ioserv/done/RewardsDashboard.java
@@ -0,0 +1,14 @@
+package com.ioserv.done;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RewardsDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewards_dashboard);
+    }
+}
