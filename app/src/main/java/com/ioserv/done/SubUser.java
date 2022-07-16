54a2f066fe4751d490b7d82eb5c5d7e1732905db
Ozan Hasdemir <200514666@student.georgianc.on.ca>
2022-07-16 15:21:56 -0400

    SubUser activity created

---
+++ app/src/main/java/com/ioserv/done/SubUser.java
@@ -0,0 +1,14 @@
+package com.ioserv.done;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_user);
+    }
+}
